00b4271492977a9984008bd333de7dccbc1b0092
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-08-08 18:04:59 +0300

    v3

--- src/tasks/components/TasksList.jsx
+++ src/tasks/components/TasksList.jsx
@@ -2,65 +2,20 @@ import React, { Component, useEffect } from 'react';
 import Task from './Task';
 import CreateTaskInput from './CreateTaskInput';
 import PropTypes from 'prop-types';
-import {
-  createTask,
-  // fetchTasksList,
-  updateTask,
-  deleteTask,
-} from '../../tasks/tasksGateway';
 import { connect } from 'react-redux';
 import * as tasksAction from '../tasks.actions';
-import { tasksListSelector } from '../tasks.selectors';
+import { sortedTasksListSelector } from '../tasks.selectors';
 
-const TasksList = ({ tasks, getTasksList }) => {
+const TasksList = ({
+  tasks,
+  getTasksList,
+  createTask,
+  updateTask,
+  deleteTask,
+}) => {
   useEffect(() => {
     getTasksList();
   }, []);
-
-  // state = {
-  //   tasks: [],
-  // };
-
-  // componentDidMount() {
-  //   this.props.getTasksList();
-  // this.fetchTasks();
-  // }
-
-  // fetchTasks = () => {
-  //   fetchTasksList().then((tasksList) =>
-  //     this.setState({
-  //       tasks: tasksList,
-  //     })
-  //   );
-  // };
-
-  // onCreate = (text) => {
-  //   const newTask = {
-  //     text,
-  //     done: false,
-  //   };
-
-  //   createTask(newTask).then(() => this.fetchTasks());
-  // };
-
-  // handleTaskStatusChange = (id) => {
-  //   const { done, text } = this.state.tasks.find((task) => task.id === id);
-
-  //   const updatedTask = {
-  //     text,
-  //     done: !done,
-  //   };
-
-  //   updateTask(id, updatedTask).then(() => this.fetchTasks());
-  // };
-
-  // handleTaskDelete = (id) => {
-  //   deleteTask(id).then(() => this.fetchTasks());
-  // };
-
-  // render() {
-  // const sortedList = this.state.tasks.slice().sort((a, b) => a.done - b.done);
-  // console.log(sortedList);
 
   return (
     <main className="todo-list">
@@ -78,25 +33,66 @@ const TasksList = ({ tasks, getTasksList }) => {
     </main>
   );
 };
-// }
-// }
-
-const mapDispatch = {
-  getTasksList: tasksAction.getTasksList,
-};
 
 TasksList.propTypes = {
   getTasksList: PropTypes.func.isRequired,
-  // id: PropTypes.string.isRequired,
-  // text: PropTypes.string.isRequired,
-  // done: PropTypes.bool.isRequired,
-  // tasks: PropTypes.arrayOf(PropTypes.shape()),
+  updateTask: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
+  createTask: PropTypes.func.isRequired,
+  tasks: PropTypes.arrayOf(PropTypes.shape()),
+};
+
+const mapDispatch = {
+  getTasksList: tasksAction.getTasksList,
+  updateTask: tasksAction.updateTask,
+  deleteTask: tasksAction.deleteTask,
+  createTask: tasksAction.createTask,
 };
 
 const mapState = (state) => {
   return {
-    tasks: tasksListSelector(state),
+    tasks: sortedTasksListSelector(state),
   };
 };
 
 export default connect(mapState, mapDispatch)(TasksList);
+
+// state = {
+//   tasks: [],
+// };
+
+// componentDidMount() {
+//   this.props.getTasksList();
+// this.fetchTasks();
+// }
+
+// fetchTasks = () => {
+//   fetchTasksList().then((tasksList) =>
+//     this.setState({
+//       tasks: tasksList,
+//     })
+//   );
+// };
+
+// onCreate = (text) => {
+//   const newTask = {
+//     text,
+//     done: false,
+//   };
+
+//   createTask(newTask).then(() => this.fetchTasks());
+// };
+
+// handleTaskStatusChange = (id) => {
+//   this.props.updateTask(id);
+// };
+
+// handleTaskDelete = (id) => {
+//   this.props.deleteTask(id)
+// };
+
+// render() {
+// const sortedList = this.state.tasks.slice().sort((a, b) => a.done - b.done);
+// console.log(sortedList);
+// }
+// }
--- src/tasks/tasks.actions.js
+++ src/tasks/tasks.actions.js
@@ -1,7 +1,9 @@
-import { fetchTasksList } from './tasksGateway';
+import * as tasksGateway from './tasksGateway';
+import { tasksListSelector } from './tasks.selectors';
+
 export const TASKS_LIST_RECIEVED = 'TASKS_LIST_RECIEVED';
 
-export const tasksListRecieved = tasksList => {
+export const tasksListRecieved = (tasksList) => {
   const action = {
     type: TASKS_LIST_RECIEVED,
     payload: {
@@ -13,12 +15,50 @@ export const tasksListRecieved = tasksList => {
 
 export const getTasksList = () => {
   const thunkAction = function (dispatcht) {
-    fetchTasksList().then((tasksList) =>
-      dispatcht(tasksListRecieved(tasksList))
-    );
+    tasksGateway
+      .fetchTasksList()
+      .then((tasksList) => dispatcht(tasksListRecieved(tasksList)));
   };
 
   return thunkAction;
 };
 
+export const updateTask = (taskid) => {
+  const thunkAction = function (dispatcht, getState) {
+    const state = getState();
+    const tasksList = tasksListSelector(state);
+    const task = tasksList.find((task) => task.id === taskid);
 
+    const updatedTask = {
+      ...task,
+      done: !task.done,
+    };
+
+    tasksGateway
+      .updateTask(taskId, updatedTask)
+      .then(() => dispatcht(getTasksList()));
+  };
+
+  return thunkAction;
+};
+
+export const deleteTask = (taskid) => {
+  const thunkAction = function (dispatcht) {
+    tasksGateway.deleteTask(taskId).then(() => dispatcht(getTasksList()));
+  };
+
+  return thunkAction;
+};
+
+export const createTask = (text) => {
+  const thunkAction = function (dispatcht) {
+    const taskData = {
+      text,
+      done: false,
+      createdAt: new Date().toISOString(),
+    };
+    tasksGateway.createTask(taskData).then(() => dispatcht(getTasksList()));
+  };
+
+  return thunkAction;
+};
